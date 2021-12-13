53cb33b62cb52ba9b4b52154cdd0d5e16de75b6a
Daph21 <82120108+Daph21@users.noreply.github.com>
2021-12-13 16:50:10 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.showString("avance")
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.showString("stop")
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.showString("recule")
+})
+basic.showString("DC motor")
